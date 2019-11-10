aa1def224c34e59d3234527bbcd50d2a9b2ed24b
Nathan Tisdale <natisdale@mail.csuchico.edu>
2019-11-10 03:00:11 +0000

    insertartistform.php created

---
+++ insertartistform.php
@@ -0,0 +1,18 @@
+<html>
+<header></header>
+<body>
+<h1>New Artist</h1>
+<form method="post" action="insertartist.php">
+	<p>id : <input type="text" name="id" placeholder="Id" /><br /></p>
+	<p>Name : <input type="text" name="name" placeholder="Name" /><br /></p>
+	<p>Bio : <input type="text" name="bio" placeholder="Bio" /><br /></p>
+	<p>Nationality : <input name="nationality" placeholder="Nationality" /><br /></p>
+	<p>Gender : <input name="gender" placeholder="Gender" /><br /></p>
+	<p>Birth : <input name="birth" placeholder="Year" /><br /></p>
+	<p>Death : <input name="death" placeholder="Year" /><br /></p>
+	<p>Wiki : <input name="wiki" placeholder="Article ID" /><br /></p>
+	<p>ULAN : <input name="ulan" placeholder="ulan" /><br /></p>
+	<p><input type="submit" name="submit" value="submit" /></p>
+</form>
+</body>
+</html>
